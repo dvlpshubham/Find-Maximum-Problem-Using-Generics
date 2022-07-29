06645dca950add9337e6a7b4ed02938326a7d8e3
Shubham Raj <shubhamaryan805@gmail.com>
2022-07-29 13:53:42 +0530

    Added : Extend the max method to take more than three parameters

--- com/generic/Main.java
+++ com/generic/Main.java
@@ -1,14 +1,14 @@
 package com.generic;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to check max of three variables");
-        String x = "jack", y = "king", z = "queen";
+        String x = "jack", y = "king", z = "queen",m="rama";
         Integer a = 25, b = 24, c = 19;
         Float p = 1.23f, q = 2.2f, r = 0.2f;
-        TestMaximum.<String>testMaximum(x, y, z);
+        TestMaximum.<String>testMaximum(x, y, z,m);
         TestMaximum.<Integer>testMaximum(a, b, c);
         TestMaximum.<Float>testMaximum(p, q, r);
     }
-
 }
